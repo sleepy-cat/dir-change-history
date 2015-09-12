ba9d1eccef553a680cb9b9857bb01c6c8cdd2f1b
dc <username@users.noreply.github.com>
2015-09-12 23:19:58 +0300

    Show number of files per time-group

--- dir-change-history.py
+++ dir-change-history.py
@@ -40,13 +40,32 @@ def iterFiles(options, roots):
                 mtime = max(stat.st_mtime, stat.st_ctime)
                 yield mtime, stat.st_size, path  # Yield file
 
+def parseOptions():
+    parser = optparse.OptionParser(usage='Usage: %prog [options] path [path2 ...]')
+    parser.add_option('-g', action='store', type='long', dest='secs', default=10,
+                      help='set threshold for grouping files')
+    parser.add_option('-f', action='append', type='string', dest='exc_files', default=[],
+                      help='exclude files matching a wildcard pattern')
+    parser.add_option('-d', action='append', type='string', dest='exc_dirs', default=[],
+                      help='exclude directories matching a wildcard pattern')
+    options, roots = parser.parse_args()
+
+    if len(roots) == 0:
+        print('You must specify at least one path.\n')
+        parser.print_help()
+
+    return options, roots
+
 
 # Build file list, sort it and dump output
 ptime = 0
-for mtime, size, path in sorted(iterFiles(options, roots), reverse=True):
+count = 0
+for mtime, size, path in sorted(iterFiles(*parseOptions()), reverse=True):
     if ptime - mtime >= options.secs:
-        print('-' * 30)
+        print('%s %d file(s)' % ('-' * 30, count))
+        count = 0
     timeStr = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(mtime))
     print('%s %10s %s' % (timeStr, size, path))
     ptime = mtime
+    count += 1
 
